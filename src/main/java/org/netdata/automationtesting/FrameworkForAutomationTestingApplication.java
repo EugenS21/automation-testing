9be3348a97c60779bfc685c5db7f6072aa71b637
EugenS21 <Rzl7kxJJ^%3QL^D70zC2>
2022-01-29 15:02:44 +0200

    * Added project files

---
+++ src/main/java/org/netdata/automationtesting/FrameworkForAutomationTestingApplication.java
@@ -0,0 +1,13 @@
+package org.netdata.automationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkForAutomationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkForAutomationTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/netdata/automationtesting/FrameworkForAutomationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.netdata.automationtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkForAutomationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
